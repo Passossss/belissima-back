ec40b9b093db8237da284aeca12054d7867f162d
Gustavo Passos <gusapas26@gmail.com>
2025-07-30 15:36:53 -0300

    migration add

--- Services/RevendedoraService.cs
+++ Services/RevendedoraService.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using belissima_back.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace belissima_back.Services
+namespace belissima_back.Services;
+
+public class RevendedoraService : IRevendedoraService
 {
-    public class RevendedoraService : Controller
+    private readonly AppDbContext _context;
+
+    public RevendedoraService(AppDbContext context)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _context = context;
     }
+
+    public async Task<IEnumerable<Revendedora>> GetAllAsync()
+    {
+        return await _context.Revendedoras.ToListAsync();
+    }
+
+    public async Task<Revendedora?> GetByIdAsync(int id)
+    {
+        return await _context.Revendedoras.FindAsync(id);
+    }
+
+
 }
