9be36bf2c03cfef25486534c360e95a1420958f2
Gustavo Passos <gusapas26@gmail.com>
2025-07-30 15:53:00 -0300

    get e get by id

--- Program.cs
+++ Program.cs
@@ -1,20 +1,12 @@
 using belissima_back;
-using belissima_back.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddScoped<IRevendedoraService, RevendedoraService>();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddCors(options =>
 {
--- Routes/RevendedoraRoutes.cs
+++ Routes/RevendedoraRoutes.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
+using belissima_back.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace belissima_back.Routes
 {
@@ -8,6 +10,13 @@ namespace belissima_back.Routes
         {
             var group = app.MapGroup("api/Revendedoras").WithTags("Revendedoras");
 
+            group.MapGet("/", async (AppDbContext db) =>
+            await db.Revendedoras.ToListAsync());
+
+            group.MapGet("/{id}", async (int id, AppDbContext db) =>
+            await db.Revendedoras.FindAsync(id) is Revendedora revendedora
+                ? Results.Ok(revendedora)
+                : Results.NotFound());
             return app;
         }
     }
